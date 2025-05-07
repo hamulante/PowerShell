f75e8b98604d68fb8bd34ce07ad94e854fe5c870
hamulante <32925718+hamulante@users.noreply.github.com>
2025-05-07 16:19:49 +0800

    Hello, World!
    check comments inside

---
+++ Hello World.ps1
@@ -0,0 +1,6 @@
+Echo "hello world!"
+
+# 2 ways to run:
+# .\"Hello World.ps1" - no space between .\ and file name 
+# & "C:\Users\beibeihu\Desktop\Hello World.ps1" - & followed by full path and quote if there is blank space
+# if it doesnt allow you to to run, run Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
